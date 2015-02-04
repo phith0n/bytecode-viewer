65c17d9560e5145d540282703ecd53be6dc51ce9
Kalen Kinloch <konloch@gmail.com>
2015-02-04 07:06:04 -0800

    2.8.0
    02/01/2015 - Updated CFR and Proycon to latest versions.
    02/01/2015 - Started working on implementing Krakatau.
    02/01/2015 - Sexifixed the security manager a little bit.
    02/03/2015 - Fully added Krakatau Java decompiler, just
    disassembly/assembly left.
    02/03/2015 - Updated the about window.
    02/03/2015 - Dropped JRuby and Jython support (BCV is now roughly 16mb,
    was 45mb).
    02/04/2015 - Added Krakatau Disassembly.
    02/04/2015 - Added Krakatau Assembly.

--- src/the/bytecode/club/bytecodeviewer/compilers/SmaliAssembler.java
+++ src/the/bytecode/club/bytecodeviewer/compilers/SmaliAssembler.java
@@ -1,80 +1,19 @@
-package the.bytecode.club.bytecodeviewer.decompilers;
+package the.bytecode.club.bytecodeviewer.compilers;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
-import me.konloch.kontainer.io.DiskReader;
 import me.konloch.kontainer.io.DiskWriter;
-
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.tree.ClassNode;
-
 import the.bytecode.club.bytecodeviewer.BytecodeViewer;
 import the.bytecode.club.bytecodeviewer.Dex2Jar;
+import the.bytecode.club.bytecodeviewer.MiscUtils;
 import the.bytecode.club.bytecodeviewer.ZipUtils;
 
-public class Smali {
-	
-	public static String decompileClassNode(ClassNode cn) {
-		final ClassWriter cw = new ClassWriter(0);
-		cn.accept(cw);
-
-		String fileStart = BytecodeViewer.tempDirectory + BytecodeViewer.fs
-				+ "temp";
-		
-		String start = JavaDecompiler.getUniqueName(fileStart, ".class");
-
-		final File tempClass = new File(start + ".class");
-		final File tempZip = new File(start + ".jar");
-		final File tempDex = new File(start + ".dex");
-		final File tempSmali = new File(start + "-smali"); //output directory
-		
-		try {
-			final FileOutputStream fos = new FileOutputStream(tempClass);
-
-			fos.write(cw.toByteArray());
-
-			fos.close();
-		} catch (final IOException e) {
-			new the.bytecode.club.bytecodeviewer.api.ExceptionUI(e);
-		}
-		
-		ZipUtils.zipFile(tempClass, tempZip);
-		Dex2Jar.saveAsDex(tempZip, tempDex);
-		try {
-			org.jf.baksmali.main.main(new String[]{"-o", tempSmali.getAbsolutePath(), "-x", tempDex.getAbsolutePath()});
-		} catch (Exception e) {
-			new the.bytecode.club.bytecodeviewer.api.ExceptionUI(e);
-		}
-		
-		File outputSmali = null;
-		
-		boolean found = false;
-		File current = tempSmali;
-		while(!found) {
-			File f = current.listFiles()[0];
-			if(f.isDirectory())
-				current = f;
-			else {
-				outputSmali = f;
-				found = true;
-			}
-				
-		}
-		try {
-			return DiskReader.loadAsString(outputSmali.getAbsolutePath());
-		} catch (Exception e) {
-			new the.bytecode.club.bytecodeviewer.api.ExceptionUI(e);
-		}
-		
-		return null;
-	}
+public class SmaliAssembler {
 	
 	public static byte[] compile(String contents) {
 		String fileStart = BytecodeViewer.tempDirectory + BytecodeViewer.fs
 				+ "temp";
-		int fileNumber = JavaDecompiler.getClassNumber(fileStart, ".dex");
+		int fileNumber = MiscUtils.getClassNumber(fileStart, ".dex");
 
 		final File tempSmaliFolder = new File(fileStart + fileNumber + "-smalifolder"+BytecodeViewer.fs);
 		tempSmaliFolder.mkdir();
